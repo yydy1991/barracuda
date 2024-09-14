8faff81806b77091f5100f2aa08e4d604a04765f
yydy1991 <151978839+yydy1991@users.noreply.github.com>
2024-09-14 08:01:00 +0800

    添加flyway模块

---
+++ barracuda-flyway/src/test/java/com/barracuda/barracudaflyway/BarracudaFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barracuda.barracudaflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarracudaFlywayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
